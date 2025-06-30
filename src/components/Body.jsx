89cbc550e20110bb09429c9322c8adc39812d8fc
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-07-01 00:06:14 +0530

    redirect to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(API_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        // User is not authenticated, redirect to login
+        navigate("/login");
+      }
+      console.error("Error fetching user data:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const user = useSelector((store) => store.user);
   console.log(user);
 
@@ -8,20 +10,22 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">
+            DevTinder
+          </Link>
         </div>
         {user && (
           <div className="flex gap-2">
             <div className="form-control flex items-center mx-1.5">
               Welcome, {user.firstName}
             </div>
-            <div className="dropdown dropdown-end">
+            <div className="dropdown dropdown-end pr-4">
               <div
                 tabIndex={0}
                 role="button"
                 className="btn btn-ghost btn-circle avatar"
               >
-                <div className="w-10 rounded-full mr-2.5">
+                <div className="w-10 rounded-full">
                   <img
                     alt="User Profile"
                     src={
@@ -36,7 +40,9 @@ const Navbar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">Profile</a>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
